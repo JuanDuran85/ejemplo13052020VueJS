3935a9da48c352d16b3651e161d96ffebdb3f629
JuanDuran85 <duran85juan@gmail.com>
2020-05-13 10:50:24 -0400

    commit 1

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,22 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="container">
+    <h1>Muestra de Series</h1>
+    <series></series>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+//import axios from 'axios';
+import Series from './components/Series.vue';
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Series
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
---
+++ src/components/Series.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <div class="card">
+      <img src="https://es.vuejs.org/images/logo.png?_sw-precache=cf23526f451784ff137f161b8fe18d5a" class="card-img-top" alt="">
+      <div class="card-body">
+        <h5 class="card-title">Card title</h5>
+        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+        <a href="#" class="btn btn-primary">Go somewhere</a>
+    </div>
+</div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Series',
+}
+</script>
+
+<style scoped>
+
+</style>
